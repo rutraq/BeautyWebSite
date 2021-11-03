8642c6a5ca89f93a6a5d66c1cc1d2b8733c6800e
rutraq <a3332133@gmail.com>
2021-11-03 14:45:26 +0100

    gallery js

--- scripts/galleryHomePage.js
+++ scripts/galleryHomePage.js
@@ -3,25 +3,27 @@ let right = $("#galleryMainPage .right")
 right.click(animatePhotosRight);
 left.click(animatePhotosLeft);
 let circle = 3;
+let photos = ["photo1", "photo2", "photo3", "photo4", "photo5"];
 
 function animatePhotosRight() {
-    if (circle < 5) {
-        let photos = ["photo1", "photo2", "photo3", "photo4", "photo5"];
+    if (circle > 1) {
         for (let i = 0; i < photos.length; i++) {
-            $(`#${photos[i]}`).css("position", "relative").animate({left: "+=150px"}, 500);
+            $(`#${photos[i]}`).css("position", "relative").animate({left: "+=300px"}, 500);
         }
-        $(`#photo${circle} img`).css({width: "-=200px"});
-        changeCircles(1);
+        $(`#photo${circle} img`).css("width", "328px").css("height", "390px");
+        $(`#photo${circle-1} img`).css({width: "504px", height: "522px"});
+        changeCircles(-1);
     }
 }
 
 function animatePhotosLeft() {
-    if (circle > 1) {
-        let photos = ["firstPhoto", "secondPhoto", "thirdPhoto", "fourthPhoto", "fifthPhoto"];
+    if (circle < 5) {
         for (let i = 0; i < photos.length; i++) {
-            $(`#${photos[i]}`).css("position", "relative").animate({left: "-=150px"}, 500);
+            $(`#${photos[i]}`).css("position", "relative").animate({left: "-=300px"}, 500);
         }
-        changeCircles(-1);
+        $(`#photo${circle} img`).css({width: "328px", height: "390px"});
+        $(`#photo${circle+1} img`).css({width: "504px", height: "522px"});
+        changeCircles(1);
     }
 }
 
@@ -29,15 +31,15 @@ function changeCircles(n) {
     $(".circles .active").removeClass("active");
     let previousCircle = circle;
     circle += n;
-    if (circle === 5) {
+    if (circle === 1) {
         right.addClass("inactive").css("cursor", "default");
-    } else if (circle === 1) {
+    } else if (circle === 5) {
         left.addClass("inactive").css("cursor", "default");
     }
     if (previousCircle === 5 && circle === 4) {
-        right.removeClass("inactive").css("cursor", "pointer");
-    } else if (previousCircle === 1 && circle === 2) {
         left.removeClass("inactive").css("cursor", "pointer");
+    } else if (previousCircle === 1 && circle === 2) {
+        right.removeClass("inactive").css("cursor", "pointer");
     }
     $(`.circles #circle${circle}`).addClass("active");
 }
