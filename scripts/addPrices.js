836d0064f4f1e6db58a8f01bedf392985fe930bb
rutraq <a3332133@gmail.com>
2021-11-14 18:37:44 +0100

    add prices for first and second article

---
+++ scripts/addPrices.js
@@ -0,0 +1,74 @@
+let facial = [
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "10$"},
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "50$"},
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "13$"},
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "22$"},
+    {"name": "Test Test Test", "price": "28$"},
+    {"name": "Test Test Test Test Test Test", "price": "90$"},
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "120$"}
+];
+let lipoSuction = [
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "13$"},
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "22$"},
+    {"name": "Test Test Test", "price": "28$"},
+    {"name": "Test Test Test Test Test Test", "price": "90$"},
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "120$"}
+];
+let breastSurgery = [
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "13$"},
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "22$"},
+    {"name": "Test Test Test", "price": "28$"},
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "13$"},
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "22$"},
+    {"name": "Test Test Test", "price": "28$"}
+];
+let otherSurgery = [
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "13$"},
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test", "price": "22$"},
+    {"name": "Test Test Test", "price": "28$"},
+    {"name": "Test Test Test Test Test Test", "price": "90$"},
+    {"name": "Test Test Test", "price": "25$"},
+    {"name": "Test Test Test Test Test Test Test Test Test Test", "price": "120$"}
+];
+let otherServices = [
+    {"name": "Change of the dressing", "price": "30$"},
+    {"name": "Stitch removal", "price": "30$"},
+    {"name": "General blood test", "price": "from 35$"}
+];
+
+$(document).ready(function () {
+    addPrice(facial, "facialSurgery");
+    addPrice(lipoSuction, "lipoSuction");
+    addPrice(breastSurgery, "breastSurgery");
+    addPrice(otherSurgery, "otherSurgery");
+    addPrice(otherServices, "otherServices");
+
+    function addPrice(db, ulId) {
+        for (let x = 0; x < db.length; x++) {
+            let serializedUser = JSON.stringify(db[x]);
+            let user = JSON.parse(serializedUser);
+            let li = document.createElement("li");
+            let name = document.createElement("div");
+            let price = document.createElement("div");
+            name.innerText = user.name;
+            price.innerText = user.price;
+            li.append(name);
+            li.append(price);
+            $(`#${ulId} ul`).append(li);
+        }
+    }
+})
