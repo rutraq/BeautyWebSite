6ee6ec67abc3c4bbc29c7b36b9fbdb553624d439
rutraq <a3332133@gmail.com>
2021-12-02 19:31:10 +0100

    make login

--- scripts/upload_text/about.js
+++ scripts/upload_text/about.js
@@ -1,6 +1,10 @@
 $(document).not(function () {
     $.getJSON('http://localhost:5000/get-text?page=about', function(data) {
-        $("#firstArticle .bigFont").text(data.about_article1_slogan);
-        $("#firstArticle .smallFont").text(data.about_article1);
+        $("#firstArticle .bigFont").text(data.article1_slogan);
+        $("#firstArticle .smallFont").text(data.article1);
+        $("#secondArticle .bigFont").text(data.article2_slogan);
+        $("#secondArticle .smallFont").text(data.article2);
+        $("#thirdArticle .bigFont").text(data.article3_slogan);
+        $("#thirdArticle .smallFont").text(data.article3);
     });
 })
